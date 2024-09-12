2c256b3741a2fc5842093a12fb0080a7c38fe25a
takaki_makoto <takaki.makoto@alhinc.jp>
2024-09-12 13:40:44 +0900

    データ回り少し修正

--- BookStore/src/main/resources/database/data.sql
+++ BookStore/src/main/resources/database/data.sql
@@ -5,8 +5,8 @@ INSERT INTO users (
 	password
 ) VALUES (
 	'0806d989-3804-4645-a2ef-9c0d76513df2',
-	'ダミー ユーザー1',
-	'example@email.com',
+	'ダミー ユーザーA',
+	'exampleA@email.com',
 	'password'
 ) ON DUPLICATE KEY UPDATE id = id^;
 
@@ -17,8 +17,8 @@ INSERT INTO users (
 	password
 ) VALUES (
 	'6f78de77-66f4-4021-a744-5fb9a5437e7b',
-	'ダミー ユーザー2',
-	'example2@email.com',
+	'ダミー ユーザーB',
+	'exampleB@email.com',
 	'password'
 ) ON DUPLICATE KEY UPDATE id = id^;
 
@@ -29,8 +29,8 @@ INSERT INTO users (
 	password
 ) VALUES (
 	'946792a2-3cc0-40e7-a874-06d6c60a761c',
-	'ダミー ユーザー3',
-	'example3@email.com',
+	'ダミー ユーザーC',
+	'exampleC@email.com',
 	'password'
 ) ON DUPLICATE KEY UPDATE id = id^;
 
@@ -41,8 +41,8 @@ INSERT INTO users (
 	password
 ) VALUES (
 	'cfe94e52-32b6-48af-ac96-2b92b0f989c5',
-	'ダミー ユーザー4',
-	'example4@email.com',
+	'ダミー ユーザーD',
+	'exampleD@email.com',
 	'password'
 ) ON DUPLICATE KEY UPDATE id = id^;
 
--- BookStore/src/main/resources/database/schema.sql
+++ BookStore/src/main/resources/database/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS users (
 	name VARCHAR(20) NOT NULL,
 	email_address VARCHAR(256) NOT NULL UNIQUE,
 	password VARCHAR(256) NOT NULL,
-	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+	created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
 )^;
 
 
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS users (
 DROP PROCEDURE IF EXISTS insert_user^;
 
 CREATE PROCEDURE insert_user (
-    IN p_name VARCHAR(20),
-    IN p_email_address VARCHAR(256),
-    IN p_password VARCHAR(256)
+--    IN p_name VARCHAR(20),
+--    IN p_email_address VARCHAR(256),
+--    IN p_password VARCHAR(256)
 )
 BEGIN
     DECLARE user_count INT;
@@ -36,9 +36,9 @@ BEGIN
 			password
 		) VALUES (
 			UUID(),
-			CONCAT(p_name, user_count + 1),
-			CONCAT(p_email_address, user_count + 1),
-			p_password
+			CONCAT('ダミー ユーザー', user_count + 1),
+			CONCAT('example', user_count + 1, '@email.com'),
+			'password'
 		);
 		
 		SET user_count = user_count + 1;
@@ -46,6 +46,7 @@ BEGIN
 	
 END ^;
 
-CALL insert_user('ダミー ユーザー', 'example@email.com', 'password')^;
+--CALL insert_user('ダミー ユーザー', 'example@email.com', 'password')^;
+CALL insert_user()^;
 
 
