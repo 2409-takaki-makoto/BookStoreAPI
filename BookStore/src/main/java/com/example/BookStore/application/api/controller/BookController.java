68b4b67c3bb8b6d41676a636faf8792e949e7fe9
takaki_makoto <takaki.makoto@alhinc.jp>
2024-09-13 13:56:27 +0900

    コントローラー作成

---
+++ BookStore/src/main/java/com/example/BookStore/application/api/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.example.BookStore.application.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/books", "/books/"})
+public class BookController {
+
+	
+}
