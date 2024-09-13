04fc6a811a7ff67039c352cf383b4a3ecfd3603e
takaki_makoto <takaki.makoto@alhinc.jp>
2024-09-13 13:54:53 +0900

    ユーザーIDコード修正

--- BookStore/src/main/java/com/example/BookStore/domain/values/UserId.java
+++ BookStore/src/main/java/com/example/BookStore/domain/values/UserId.java
@@ -4,46 +4,43 @@ import java.util.Objects;
 import java.util.UUID;
 
 public record UserId(String value) {
-	
+
 	public UserId(String value) {
-		
+
 		if (value == null) {
 			UUID uuid = UUID.randomUUID();
 			value = uuid.toString();
 		}
-		
+
 		this.value = value;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null) {
 			return Boolean.FALSE;
 		}
-		
+
 		if (Objects.equals(this, obj)) {
 			return Boolean.TRUE;
 		}
-		
+
 		if (!(obj instanceof UserId)) {
 			return Boolean.FALSE;
 		}
-		
-		UserId otherObj = (UserId)obj;
-		
+
+		UserId otherObj = (UserId) obj;
+
 		if (this.value == null) {
 			if (otherObj.value == null) {
 				return Boolean.TRUE;
 			}
-			
+
 			return Boolean.FALSE;
 		}
-		
-		if (this.value.equals(otherObj.value)) {
-			return Boolean.TRUE;
-		}
-		
-		return Boolean.FALSE;
+
+		return this.value.equals(otherObj.value);
+
 	}
-	
+
 }
